29be80ea141a6351c9b6c47d6d134fd5dfe6f1da
Cédric GOUJON <40459108+HrexD@users.noreply.github.com>
2024-03-22 16:22:42 +0100

    raaaaaaaaaaaaaaah

--- app.py
+++ app.py
@@ -2,15 +2,9 @@ from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-@app.route('/get', methods=['GET'])
-def get_endpoint():
-    
+@app.route('/monsupertravaildelegende', methods=['GET'])
+def monsupertravail():
     return f"Bonjour, j'aimerais que ca marche"
-
-@app.route('/post', methods=['POST'])
-def post_endpoint():
-    data = request.get_json()
-    return f"POST endpoint called with data: {data}"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
